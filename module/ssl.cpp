87aea284e939cb972534a9465b7eaa00c8b6ac90
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2024-04-05 23:39:28 +0800

    added module support.

---
+++ module/ssl.cpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2024 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+module;
+#include <boost/beast/ssl.hpp>
+
+export module boost.beast.ssl ;
+
+namespace boost::beast
+{
+// SSL
+export using boost::beast::ssl_stream;
+
+}
